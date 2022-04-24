7fae20c1f569e5a2383065f7ae16c771e80aa746
Marcos Nicolau <69830188+oScanner@users.noreply.github.com>
2022-04-24 16:45:07 -0300

    HawkTech - Update 0.5
    Navbar finally ready
    - Icon fixed
    - Menu itens fixed
    - Submenu on click added
    - Sections of submenu: Computadores, Hardware, Periféricos  and Consoles
    - Login button fixed
    Navbar = Finished

---
+++ ecommerce/static/js/dropdown.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+ var botao = $('.category');
+ var dropDown = $('.sub-menu-1');    
+
+    botao.on('click', function(event){
+        dropDown.stop(true,true).slideToggle();
+        event.stopPropagation();
+    });
+});
+
+$(document).ready(function() {
+ var botao = $('.category1');
+ var dropDown = $('#pcs');    
+
+    botao.on('click', function(event){
+        dropDown.stop(true,true).slideToggle();
+        event.stopPropagation();
+    });
+});
+$(document).ready(function() {
+ var botao = $('.category2');
+ var dropDown = $('#har');    
+
+    botao.on('click', function(event){
+        dropDown.stop(true,true).slideToggle();
+        event.stopPropagation();
+    });
+});
+
+$(document).ready(function() {
+ var botao = $('.category3');
+ var dropDown = $('#per');    
+
+    botao.on('click', function(event){
+        dropDown.stop(true,true).slideToggle();
+        event.stopPropagation();
+    });
+});
+
+$(document).ready(function() {
+ var botao = $('.categoryCPU');
+ var dropDown = $('#cpu');    
+
+    botao.on('click', function(event){
+        dropDown.stop(true,true).slideToggle();
+        event.stopPropagation();
+    });
+});
+$(document).ready(function() {
+ var botao = $('.categoryMOB');
+ var dropDown = $('#mob');    
+
+    botao.on('click', function(event){
+        dropDown.stop(true,true).slideToggle();
+        event.stopPropagation();
+    });
+});
+$(document).ready(function() {
+ var botao = $('.categoryGPU');
+ var dropDown = $('#gpu');    
+
+    botao.on('click', function(event){
+        dropDown.stop(true,true).slideToggle();
+        event.stopPropagation();
+    });
+});
+
+$(document).ready(function() {
+ var botao = $('.categoryDISK');
+ var dropDown = $('#disk');    
+
+    botao.on('click', function(event){
+        dropDown.stop(true,true).slideToggle();
+        event.stopPropagation();
+    });
+});
+
+$(document).ready(function() {
+ var botao = $('.categoryCOOL');
+ var dropDown = $('#cool');    
+
+    botao.on('click', function(event){
+        dropDown.stop(true,true).slideToggle();
+        event.stopPropagation();
+    });
+});
+
